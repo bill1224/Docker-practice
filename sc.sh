cf14496defedb605d937ddf6a5813950e2f73744
jongin <bill1224@naver.com>
2021-06-04 16:55:15 +0900

    shell scrip

--- sc.sh
+++ sc.sh
@@ -2,19 +2,28 @@
 
 
 PRE_IFS=$IFS
+TOT=0
 IFS="
 "
+
+user=`whoami`
+FileName="bin_files.txt"
+cd /home/${user}/workspace/Docker-practice
+touch $FileName
 
 for i in `ls -al`; do
     S=`echo $i | awk '{print $5}'`
     F=`echo $i | awk '{print $9}'`
 
-    if [ "$F" == "." ] || [ "$F" == ".." ]; then
+    if [ "$F" == "." ] || [ "$F" == ".." ] || [ "$F" == "" ]; then
         continue
     fi
    
-    echo "$S $F"    
+    TOT=`expr $TOT + $S`
+    echo "$S $F" >> $FileName
 done
 
+
+echo "Total size is $TOT"
 IFS=$PRE_IFS
 
